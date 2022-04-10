ec05b23fe51a50ff777380c8530b731fccc1c4af
Zimmius <grybakov@usetech.ru>
2022-04-10 10:37:14 +0300

    Добавьте файлы проекта.

---
+++ addressbook_tests_autoit/appmanager/HelperBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoItX3Lib;
+
+namespace addressbook_tests_autoit
+{
+    public class HelperBase
+    {
+        protected ApplicationManager manager;
+
+        public HelperBase(ApplicationManager manager)
+        {
+
+            this.manager = manager;
+        }
+    }
+}
